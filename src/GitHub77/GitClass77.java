7869e8861d151d9000bc194f6fbd773a79609519
antonyv15 <antonyvalle15@gmail.com>
2021-08-21 20:45:33 -0400

    third commit

--- src/GitHub77/GitClass77.java
+++ src/GitHub77/GitClass77.java
@@ -3,7 +3,7 @@ package GitHub77;
 public class GitClass77 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("This is my none commit file");
 
 	}
 
--- src/GitHub77/GitClass777.java
+++ src/GitHub77/GitClass777.java
@@ -1,5 +1,8 @@
 package GitHub77;
 
 public class GitClass777 {
-
+	public static void main(String []args) {
+		System.out.println("this is the commit file");
+	
+}
 }
